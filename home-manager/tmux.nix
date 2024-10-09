6e557fb5fe1b8cb091748e145dcd4bef480707c2
Tadeas Uradnik <taduradnik@gmail.com>
2024-10-09 19:26:00 +0200

    update

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -3,6 +3,6 @@
     enable = true;
     keyMode = "vi";
     terminal = "screen-256color";
-    extraConfig = builtins.readFile ./tmux.conf;
+    catppuccin.extraConfig = builtins.readFile ./catppuccin-tmux.conf;
   };
 }
