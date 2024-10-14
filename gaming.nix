5c36195470cbd39bba86421215d919618d9c2b1a
Tadeas Uradnik <taduradnik@gmail.com>
2024-10-14 14:15:14 +0200

    update

---
+++ gaming.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    prismlauncher
+    unciv
+    steam-run
+    minecraft-server
+    openjdk17
+    openjdk8
+    lutris
+    wine64
+  ];
+  programs = {
+    steam.enable = true;
+    java.enable = true;
+    nix-ld.enable = true; # for random stuff on itch.io
+  };
+}
